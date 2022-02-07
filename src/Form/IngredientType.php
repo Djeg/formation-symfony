ce561e3a4d92ff397fb59cf1550635b053f928d4
David Jegat <david.jegat@gmail.com>
2022-02-07 14:12:36 +0100

    Ajout du formulaire des ingredients

---
+++ src/Form/IngredientType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ingredient;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class IngredientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom :',
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix :',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ingredient::class,
+            'method' => 'POST',
+        ]);
+    }
+}
