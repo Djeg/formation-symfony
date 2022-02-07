db79829f0479d5810cc64486f5792abf82939b22
David Jegat <david.jegat@gmail.com>
2022-02-07 14:36:54 +0100

    Correctin d'un mauvais import

--- src/Form/IngredientType.php
+++ src/Form/IngredientType.php
@@ -3,12 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Ingredient;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class IngredientType extends AbstractType
 {
