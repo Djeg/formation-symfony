d43ef0a339df1bdea4eb1c96848845280b653a6a
David Jegat <david.jegat@gmail.com>
2022-04-01 10:12:50 +0200

    Ajout de la recherche d'api sur les auteurs

--- src/Controller/API/AuthorController.php
+++ src/Controller/API/AuthorController.php
@@ -4,17 +4,23 @@ declare(strict_types=1);
 
 namespace App\Controller\API;
 
+use App\Form\API\ApiSearchAuthorType;
 use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorController extends AbstractController
 {
     #[Route('/api/authors', name: 'app_api_author_list', methods: ['GET'])]
-    public function list(AuthorRepository $repository): Response
+    public function list(AuthorRepository $repository, Request $request): Response
     {
-        $authors = $repository->findAll();
+        $form = $this->createForm(ApiSearchAuthorType::class);
+
+        $form->handleRequest($request);
+
+        $authors = $repository->findBySearch($form->getData());
 
         return $this->json($authors);
     }
---
+++ src/Form/API/ApiSearchAuthorType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form\API;
+
+use App\DTO\AuthorSearch;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ApiSearchAuthorType extends AbstractType
+{
+    public AuthorSearch $emptyData;
+
+    public function __construct()
+    {
+        $this->emptyData = new AuthorSearch();
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('limit', IntegerType::class, [
+                'required' => false,
+                'empty_data' => $this->emptyData->limit,
+            ])
+            ->add('page', IntegerType::class, [
+                'required' => false,
+                'empty_data' => $this->emptyData->page,
+            ])
+            ->add('sortBy', ChoiceType::class, [
+                'required' => false,
+                'choices' => [
+                    'id' => 'id',
+                    'name' => 'name',
+                ],
+                'empty_data' => $this->emptyData->sortBy,
+            ])
+            ->add('direction', ChoiceType::class, [
+                'required' => false,
+                'choices' => [
+                    'ASC' => 'ASC',
+                    'DESC' => 'DESC',
+                ],
+                'empty_data' => $this->emptyData->direction,
+            ])
+            ->add('name', TextType::class, [
+                'required' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AuthorSearch::class,
+            'method' => 'GET',
+            'csrf_protection' => false,
+            'data' => $this->emptyData,
+        ]);
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return '';
+    }
+}
