b9a69b0f905c4fbace22a7a2b55bec836c4a8442
David Jegat <david.jegat@gmail.com>
2022-04-01 10:01:16 +0200

    Ajout de doc sur les form d'api

--- src/Controller/API/BookController.php
+++ src/Controller/API/BookController.php
@@ -16,12 +16,32 @@ class BookController extends AbstractController
     #[Route('/api/books', name: 'app_api_book_list', methods: ['GET'])]
     public function list(BookRepository $repository, Request $request): Response
     {
+        /**
+         * Création d'un formulaire de recherche pour les livres :
+         * ApiSearchBookType
+         */
         $form = $this->createForm(ApiSearchBookType::class);
 
+        /**
+         * On remplie le formulaire avec les filtres (query string)
+         * préciser dans l'url
+         */
         $form->handleRequest($request);
 
+        /**
+         * On récupére les données dans la base de données,
+         * on envoie le DTO (Data Transfert Object : App\DTO\BookSearch) 
+         * à notre BookRepository.
+         * 
+         * Graçe au données contenue à l'intérieur du DTO nous pouvons
+         * rechercher des livres spécifique.
+         */
         $books = $repository->findBySearch($form->getData());
 
+        /**
+         * On transforme notre tableaux de livres en tableau
+         * JSON et nous retournons une réponse.
+         */
         return $this->json($books);
     }
 }
--- src/Form/API/ApiSearchBookType.php
+++ src/Form/API/ApiSearchBookType.php
@@ -13,18 +13,48 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ApiSearchBookType extends AbstractType
 {
+    /**
+     * On déclare une propriété public qui contient
+     * une instance du DTO du formulaire.
+     * 
+     * Cette instance est utilisé afin de récupérer les données
+     * par défaut que le formulaire doit posséder.
+     * 
+     * Example:
+     *  Si aucun limit n'est donné au formulaires, et bien
+     *  la limite de ce DTO sera utilisé (10)
+     */
     public BookSearch $emptyData;
 
+    /**
+     * On déclare un constructeur. Dans se constructeur
+     * nous assignons à notre propriété public $emptyData
+     * une instance du DTO du formulaire.
+     */
     public function __construct()
     {
         $this->emptyData = new BookSearch();
     }
 
+    /**
+     * On créé les champs du formulaire. Pour le cas d'un formulaire
+     * d'api, c'est ici qu'on spécifie tout les filtres (query string)
+     * disponible.
+     */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('limit', IntegerType::class, [
+                // Pas besoin de label car il n'y a pas de HTML
+                // donc pas de visuel.
                 'required' => false,
+                /**
+                 * Ici on spécifie la valeur utilisé si jamais
+                 * il n'y a aucune limit dans les filtres (query string).
+                 * 
+                 * Ici on s'assure que même si je n'ai aucune limit
+                 * de préciser, la valeur sera 10.
+                 */
                 'empty_data' => $this->emptyData->limit,
             ])
             ->add('page', IntegerType::class, [
@@ -71,10 +101,33 @@ class ApiSearchBookType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            /**
+             * Ici on spécifie le DTO (Data Transfert Object) ou l'entité
+             * que le formulaire doit remplir.
+             */
             'data_class' => BookSearch::class,
+            /**
+             * On spécifie la méthode HTTP que le formulaire supporte.
+             * Dans le cas d'un formulaire de recherche la méthode GET est utilisé
+             * afin de passer les champs du formulaire dans les query string
+             * (les filtres de l'url).
+             */
             'method' => 'GET',
+            /**
+             * Ici on désactive le token de protection CSRF (utilisé
+             * pour éviter la duplication malveillante d'un formulaire).
+             * 
+             * Dans le cas d'un api, les formulaires n'ont pas de HTML, donc
+             * il est impossible de le dupliquer, donc pas besoin de token.
+             */
             'csrf_protection' => false,
-            'empty_data' => $this->emptyData,
+            /**
+             * Ici on spécifie les données que le formulaire doit contenir
+             * par défaut. Lors de le création du formulaire, que nous ayons
+             * spécifié des filtres ou pas, nous aurons toujours ce DTO
+             * dans le $form->getData().
+             */
+            'data' => $this->emptyData,
         ]);
     }
 
@@ -83,6 +136,12 @@ class ApiSearchBookType extends AbstractType
      * 
      * Cela permet de mettre direction dans l'url le nom du champs
      * sans avoir à spécifier de préfix :).
+     * 
+     * Example si le préfix n'est pas desactivé :
+     *  http://mon-app.com/books?api_search_book[limit]=3
+     * 
+     * Exemple si le prefix est désactive :
+     *  http://mon-app.com/books?limit=3
      */
     public function getBlockPrefix(): string
     {
