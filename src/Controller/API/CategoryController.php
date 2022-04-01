8d9e62e990103b7e2c6f10d5ceceb953baa6f9b0
David Jegat <david.jegat@gmail.com>
2022-04-01 10:17:36 +0200

    Correction de la rechercher des catégories sur l'api

--- src/Controller/API/CategoryController.php
+++ src/Controller/API/CategoryController.php
@@ -4,17 +4,23 @@ declare(strict_types=1);
 
 namespace App\Controller\API;
 
+use App\Form\API\ApiSearchCategoryType;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
     #[Route('/api/categories', name: 'app_api_category_list', methods: ['GET'])]
-    public function list(CategoryRepository $repository): Response
+    public function list(CategoryRepository $repository, Request $request): Response
     {
-        $categories = $repository->findAll();
+        $form = $this->createForm(ApiSearchCategoryType::class);
+
+        $form->handleRequest($request);
+
+        $categories = $repository->findBySearch($form->getData());
 
         return $this->json($categories);
     }
