30271aa3ecf9cd7c6e30621e08811e3e82abddfc
David Jegat <david.jegat@gmail.com>
2022-04-06 10:13:43 +0200

    Introduit la liste des pizzas sur la page d'accueil

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Front;
 
+use App\Repository\PizzaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,11 +17,17 @@ class HomeController extends AbstractController
 {
     /**
      * Voici la méthode de controlleur affichant la 
-     * page d'accueil du site
+     * page d'accueil du site. Pour cette page 
+     * d'accueil nous récupérons la liste des dernières
+     * pizza.
      */
     #[Route('/', name: 'app_front_home_home')]
-    public function home(): Response
+    public function home(PizzaRepository $repository): Response
     {
-        return $this->render('front/home/home.html.twig');
+        $pizzas = $repository->findLatest();
+
+        return $this->render('front/home/home.html.twig', [
+            'pizzas' => $pizzas,
+        ]);
     }
 }
---
+++ src/Repository/PizzaRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Pizza;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Pizza|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Pizza|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Pizza[]    findAll()
+ * @method Pizza[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PizzaRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Pizza::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Pizza $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Pizza $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * Retourne toutes les dernières pizza trier
+     * par date de mise à jour croissante
+     */
+    public function findLatest(): array
+    {
+        return $this
+            ->createQueryBuilder('pizza')
+            ->orderBy('pizza.updatedAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+}
