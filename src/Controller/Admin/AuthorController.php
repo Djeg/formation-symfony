e4cffec5c003e90096c1be5f90b41491311f7988
David Jegat <david.jegat@gmail.com>
2022-04-11 17:04:47 +0200

    Correction du CRUD des auteurs

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use App\Entity\Author;
+use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,8 +16,67 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AuthorController extends AbstractController
 {
 	#[Route('/admin/auteurs', name: 'app_admin_author_list', methods: ['GET'])]
-	public function list(): Response
+	public function list(AuthorRepository $repository): Response
 	{
-		return new Response('test');
+		// Récupération de tout les auteurs
+		$authors = $repository->findAll();
+
+		return $this->render('admin/author/list.html.twig', [
+			'authors' => $authors,
+		]);
+	}
+
+	#[Route('/admin/auteurs/nouveau', name: 'app_admin_author_create', methods: ['GET', 'POST'])]
+	public function create(Request $request, AuthorRepository $repository): Response
+	{
+		// Création d'un formulaire :
+		$form = $this->createForm(AuthorType::class, new Author());
+
+		// On remplie notre formulaire avec les données de request si il y en as
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			// Enregistrer notre en base données
+			$repository->add($form->getData());
+
+			// Rediriger vers la liste des auteurs
+			return $this->redirectToRoute('app_admin_author_list');
+		}
+
+		// On affiche le template avec le formulaire
+		return $this->render('admin/author/create.html.twig', [
+			'form' => $form->createView(),
+		]);
+	}
+
+	#[Route('/admin/auteurs/{id}/modifier', name: 'app_admin_author_update', methods: ['GET', 'POST'])]
+	public function update(Author $author, Request $request, AuthorRepository $repository): Response
+	{
+		// Création d'un formulaire :
+		$form = $this->createForm(AuthorType::class, $author);
+
+		// On remplie notre formulaire avec les données de request si il y en as
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			// Enregistrer notre en base données
+			$repository->add($form->getData());
+
+			// Rediriger vers la liste des auteurs
+			return $this->redirectToRoute('app_admin_author_list');
+		}
+
+		// On affiche le template avec le formulaire
+		return $this->render('admin/author/update.html.twig', [
+			'form' => $form->createView(),
+		]);
+	}
+
+	#[Route('/admin/auteurs/{id}/supprimer', name: 'app_admin_author_delete')]
+	public function delete(Author $author, AuthorRepository $repository): Response
+	{
+		$repository->remove($author);
+
+		return $this->redirectToRoute('app_admin_author_list');
 	}
 }
---
+++ src/Form/AuthorType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class AuthorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom :',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description :',
+            ])
+            ->add('createdAt', DateTimeType::class, [
+                'label' => 'Date de création :',
+            ])
+            ->add('updatedAt', DateTimeType::class, [
+                'label' => 'Date de mise à jour :',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Author::class,
+        ]);
+    }
+}
