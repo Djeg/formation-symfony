addc015c35096ac062acb4782fd71aef16c8de3d
David Jegat <david.jegat@gmail.com>
2022-06-08 16:20:25 +0200

    Correction form des livres : Création du AdminBookType

---
+++ src/Form/AdminBookType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminBookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre du livre :',
+                'required' => true,
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix du livre :',
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description du livre :',
+                'required' => false,
+            ])
+            ->add('imageUrl', UrlType::class, [
+                'label' => "URL de l'image du livre :",
+                'required' => false,
+            ])
+            ->add('author', EntityType::class, [
+                'label' => "Choix de l'auteur :",
+                // Spécifie l'entité que l'on veut pouvoir séléctioner
+                'class' => Author::class,
+                // Spécifie la propriété de la class Author que l'on veut afficher
+                // ici: author.name
+                'choice_label' => 'name',
+                'required' => false,
+            ])
+            ->add('categories', EntityType::class, [
+                'label' => "Choix des catégories :",
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->add('send', SubmitType::class, [
+                'label' => 'Envoyer',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
