e5a4a3db950f4cb3bd6b26599cbc3a604f497a0e
David Jegat <david.jegat@gmail.com>
2022-06-08 14:06:50 +0200

    Correction Auteur form : Creation d'un auteur

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Entity\Author;
+use App\Form\AdminAuthorType;
 use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,28 +17,28 @@ class AuthorController extends AbstractController
     #[Route('/admin/auteurs/nouveau', name: 'app_admin_author_create')]
     public function create(Request $request, AuthorRepository $repository): Response
     {
-        // Tester si le formulaire a était envoyé
-        if ($request->isMethod('POST')) {
-            // Récupérer les données du formulaire
-            $name = $request->request->get('name');
-            $description = $request->request->get('description');
-            $imageUrl = $request->request->get('imageUrl');
+        // Création du formulaire
+        $form = $this->createForm(AdminAuthorType::class);
 
-            // Créer l'auteur à partir des données du formulaire
-            $author = new Author();
-            $author->setName($name);
-            $author->setDescription($description);
-            $author->setImageUrl($imageUrl);
+        // On remplie le formulaire avec les données envoyé par l'utilisateur
+        $form->handleRequest($request);
 
-            // enregistrer l'auteur grâce au répository
+        // Tester si le formulaire est envoyé et est valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // récupération de l'auteur
+            $author = $form->getData();
+
+            // enregistrement des données en base de données
             $repository->add($author, true);
 
-            // Rediriger l'utilisateur vers la liste des auteurs
+            // Redirection vers la page de la liste
             return $this->redirectToRoute('app_admin_author_list');
         }
 
-        // afficher le formulaire (la page twig)
-        return $this->render('admin/author/create.html.twig');
+        // Afficher la page
+        return $this->render('admin/author/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     #[Route('/admin/auteurs', name: 'app_admin_author_list')]
---
+++ src/Form/AdminAuthorType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminAuthorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => "Nom de l'auteur",
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => "Description de l'auteur",
+                'required' => false,
+            ])
+            ->add('imageUrl', UrlType::class, [
+                'label' => "URL de l'image de l'auteur",
+                'required' => false,
+            ])
+            ->add('send', SubmitType::class, [
+                'label' => 'Envoyer',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Author::class,
+        ]);
+    }
+}
