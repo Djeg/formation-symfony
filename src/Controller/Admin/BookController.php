3b80b50c1dee7e2b88cdb3ae6889967e67033be7
David Jegat <david.jegat@gmail.com>
2022-06-14 14:05:58 +0200

    Ajout d'utilisateur dans les fixtures

--- src/Controller/Admin/BookController.php
+++ src/Controller/Admin/BookController.php
@@ -7,11 +7,13 @@ namespace App\Controller\Admin;
 use App\Entity\Book;
 use App\Form\AdminBookType;
 use App\Repository\BookRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_ADMIN')]
 class BookController extends AbstractController
 {
     // Attacher une Route (OK)
