890e65f947143440661c3c46d6e2d0e772ed1f94
David Jegat <david.jegat@gmail.com>
2022-06-14 11:34:53 +0200

    Ajout du formulaire d'inscription

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email :',
+                'required' => true,
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe :',
+                ],
+                'second_options' => [
+                    'label' => 'Répéter votre mot de passe :',
+                ]
+            ])
+            ->add('send', SubmitType::class, [
+                'label' => "S'inscrire",
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
