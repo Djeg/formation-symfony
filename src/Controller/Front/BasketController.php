bc02cc00e89f45259754892fc585699d794b96f3
David Jegat <david.jegat@gmail.com>
2022-06-16 14:33:38 +0200

    Correction Panier : Finalisation des commandes

--- src/Controller/Front/BasketController.php
+++ src/Controller/Front/BasketController.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace App\Controller\Front;
 
+use App\DTO\Payment;
 use App\Entity\Book;
+use App\Entity\Order;
+use App\Entity\User;
+use App\Form\PaymentType;
+use App\Repository\AddressRepository;
 use App\Repository\BasketRepository;
+use App\Repository\OrderRepository;
+use App\Repository\UserRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -55,5 +63,56 @@ class BasketController extends AbstractController
 
         // On redirige vers la page d'affichage du panier
         return $this->redirectToRoute('app_front_basket_display');
+    }
+
+    #[Route('/mon-panier/validation', name: 'app_front_basket_validate')]
+    public function validate(Request $request, OrderRepository $orderRepository, UserRepository $userRepository, BasketRepository $basketRepository): Response
+    {
+        // création du paimeent
+        $payment = new Payment();
+
+        // Récupération de l'utilisateur connécté
+        /** @var User $user */
+        $user = $this->getUser();
+        $payment->address = $user->getDeliveryAddress();
+
+        // création du formulaire
+        $form = $this->createForm(PaymentType::class, $payment);
+
+        // On remplie le formulaire avec les données de la requête
+        $form->handleRequest($request);
+
+        // On test si le formulaire a bien était envoyé
+        if ($form->isSubmitted() && $form->isValid()) {
+            // On créer la commande
+            $order = new Order();
+
+            // On attache l'utilisateur à la commande
+            $order->setUser($user);
+
+            // On boucle sur tout les livres du panier
+            foreach ($user->getBasket()->getBooks() as $book) {
+                // On ajoute le livre a la commande
+                $order->addBook($book);
+                // On supprime le livre du panier
+                $user->getBasket()->removeBook($book);
+            }
+
+            // On ajout l'address de livraison à l'utilisateur
+            $user->setDeliveryAddress($payment->address);
+
+            // On sauvegarde le tout !
+            $userRepository->add($user);
+            $orderRepository->add($order);
+            $basketRepository->add($user->getBasket(), true);
+
+            // On redirige vers une page de détail
+            return new Response('Commande reçu avec succès !');
+        }
+
+        // On affiche la page de validation du panier
+        return $this->render('front/basket/validate.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
--- src/Form/PaymentType.php
+++ src/Form/PaymentType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\DTO\Payment;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -29,7 +30,10 @@ class PaymentType extends AbstractType
                 'label' => 'Code secret (CVC) :',
                 'required' => true,
             ])
-            ->add('address', AddressType::class);
+            ->add('address', AddressType::class)
+            ->add('send', SubmitType::class, [
+                'label' => 'Acheter',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
