563ad5f02485a533be1af3d5694ee6cefedd8fd9
David Jegat <david.jegat@gmail.com>
2022-06-16 14:09:49 +0200

    Correction Panier: Ajout du formulaire d'address

---
+++ src/Form/AddressType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('street', TextType::class, [
+                'label' => 'N° de la rue :',
+                'required' => true,
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville :',
+                'required' => true,
+            ])
+            ->add('zipCode', TextType::class, [
+                'label' => 'Code Postale :',
+                'required' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
