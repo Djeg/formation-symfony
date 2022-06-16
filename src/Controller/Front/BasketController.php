8df191e5e46199315ba05dcb03998e9354850079
David Jegat <david.jegat@gmail.com>
2022-06-16 13:49:09 +0200

    Correction panier : Suppression de livres du panier

--- src/Controller/Front/BasketController.php
+++ src/Controller/Front/BasketController.php
@@ -11,9 +11,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_USER')]
 class BasketController extends AbstractController
 {
-    #[IsGranted('ROLE_USER')]
     #[Route('/mon-panier/{id}/ajouter', name: 'app_front_basket_add')]
     public function add(Book $book, BasketRepository $repository): Response
     {
@@ -32,10 +32,28 @@ class BasketController extends AbstractController
         return $this->redirectToRoute('app_front_basket_display');
     }
 
-    #[IsGranted('ROLE_USER')]
     #[Route('/mon-panier', name: 'app_front_basket_display')]
     public function display(): Response
     {
         return $this->render('front/basket/display.html.twig');
+    }
+
+    #[Route('/mon-panier/{id}/supprimer', name: 'app_front_basket_remove')]
+    public function remove(Book $book, BasketRepository $repository): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        // On récupére le panier de l'utilisateur connécté
+        $basket = $user->getBasket();
+
+        // Suppression du livre du panier
+        $basket->removeBook($book);
+
+        // On enregistre la panier
+        $repository->add($basket, true);
+
+        // On redirige vers la page d'affichage du panier
+        return $this->redirectToRoute('app_front_basket_display');
     }
 }
