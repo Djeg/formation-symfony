378c6616f6307487ae31d2d081992b9a854fe444
David Jegat <david.jegat@gmail.com>
2022-06-17 10:06:24 +0200

    Ajout dy cryptage du mot de passe

--- src/Controller/API/UserController.php
+++ src/Controller/API/UserController.php
@@ -10,6 +10,7 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api/users')]
@@ -33,7 +34,7 @@ class UserController extends AbstractController
     }
 
     #[Route('/', name: 'app_api_user_create', methods: ['POST'])]
-    public function create(Request $request, UserRepository $repository): Response
+    public function create(Request $request, UserRepository $repository, UserPasswordHasherInterface $hasher): Response
     {
         // Création d'un formulaire
         $form = $this->createForm(RegistrationType::class);
@@ -43,11 +44,17 @@ class UserController extends AbstractController
 
         // On test si il est soumis et valide
         if ($form->isSubmitted() && $form->isValid()) {
+            // On récupére l'utilisateur créé par le formulaire
+            $user = $form->getData();
+
+            // On crypte le mot de passe
+            $user->setPassword($hasher->hashPassword($user, $user->getPassword()));
+
             // Si c'est bon, on enregistre l'utilisateur
-            $repository->add($form->getData(), true);
+            $repository->add($user, true);
 
             // On retourne l'utilisateur créé en json !
-            return $this->json($form->getData());
+            return $this->json($user);
         }
 
         // Si ce n'est pas le cas on retourne une erreur 400 avec
