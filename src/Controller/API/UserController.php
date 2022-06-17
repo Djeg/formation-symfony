4cb541b38d4933fab91608de64a0b05b1df2bd80
David Jegat <david.jegat@gmail.com>
2022-06-17 10:02:18 +0200

    Ajout de création du user en API

--- src/Controller/API/UserController.php
+++ src/Controller/API/UserController.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Controller\API;
 
 use App\Entity\User;
+use App\Form\RegistrationType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,6 +28,30 @@ class UserController extends AbstractController
     #[Route('/{id}', name: 'app_api_user_show', methods: ['GET'])]
     public function show(User $user): Response
     {
+        // On retourne l'utilisateur en JSON
         return $this->json($user);
+    }
+
+    #[Route('/', name: 'app_api_user_create', methods: ['POST'])]
+    public function create(Request $request, UserRepository $repository): Response
+    {
+        // Création d'un formulaire
+        $form = $this->createForm(RegistrationType::class);
+
+        // On remplie le formulaire
+        $form->handleRequest($request);
+
+        // On test si il est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Si c'est bon, on enregistre l'utilisateur
+            $repository->add($form->getData(), true);
+
+            // On retourne l'utilisateur créé en json !
+            return $this->json($form->getData());
+        }
+
+        // Si ce n'est pas le cas on retourne une erreur 400 avec
+        // les erreurs du formulaire en JSON
+        return $this->json($form->getErrors(), 400);
     }
 }
