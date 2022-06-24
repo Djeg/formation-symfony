38e6cb411c784bc1daf2fb34ab7015f44c007fdd
David Jegat <david.jegat@gmail.com>
2022-06-24 12:14:13 +0200

    Correction panier : Ajout des boutons "+" et "-"

--- src/Controller/BasketController.php
+++ src/Controller/BasketController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Pizza;
+use App\Repository\ArticleRepository;
 use App\Repository\BasketRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -36,6 +37,38 @@ class BasketController extends AbstractController
         $basket->addArticle($article);
 
         $repository->add($basket, true);
+
+        return $this->redirectToRoute('app_basket_display');
+    }
+
+    #[Route('/mon-panier/{id}/plus', name: 'app_basket_increase')]
+    public function increase(Article $article, ArticleRepository $repository): Response
+    {
+        $article->setQuantity($article->getQuantity() + 1);
+
+        $repository->add($article, true);
+
+        return $this->redirectToRoute('app_basket_display');
+    }
+
+    #[Route('/mon-panier/{id}/diminuer', name: 'app_basket_decrease')]
+    public function decrease(Article $article, ArticleRepository $repository, BasketRepository $basketRepo): Response
+    {
+        $article->setQuantity($article->getQuantity() - 1);
+
+        if ($article->getQuantity() <= 0) {
+            /** @var User $user */
+            $user = $this->getUser();
+            $basket = $user->getBasket();
+
+            $basket->removeArticle($article);
+
+            $basketRepo->add($basket, true);
+
+            return $this->redirectToRoute('app_basket_display');
+        }
+
+        $repository->add($article, true);
 
         return $this->redirectToRoute('app_basket_display');
     }
