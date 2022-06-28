ac3df0fd262fb1b18bb4ae9e579db9e2e04285fe
David Jegat <david.jegat@gmail.com>
2022-06-28 11:35:10 +0200

    Correction inscription : Ajout du pied de page

--- public/inscription.php
+++ public/inscription.php
@@ -94,7 +94,20 @@
     </main>
 
     <!-- le pied de page, la balise footer -->
-    <footer></footer>
+    <footer>
+        <div class="footer-content">
+            <div class="logo">PizzaShop</div>
+            <nav class="sitemap">
+                <p>Plan du site</p>
+                <ul>
+                    <li>Accueil</li>
+                    <li>Contact</li>
+                    <li>Rechercher</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    </footer>
 </body>
 
 </html>
