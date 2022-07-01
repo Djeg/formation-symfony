e54b99bd539a162b85b274c37f293f229d9191aa
David Jegat <david.jegat@gmail.com>
2022-07-01 12:35:23 +0200

    Correction de la liste des pizzas : Ajout de design

--- public/admin/list-pizza.php
+++ public/admin/list-pizza.php
@@ -14,9 +14,28 @@ $view = (new ListPizzaController())->start();
 <div class="content">
     <h1>Liste des pizzas</h1>
 
-    <? foreach ($view->list as $pizza) : ?>
-        <p><?= $pizza->name ?> (prix: <?= $pizza->price ?>)</p>
-    <? endforeach ?>
+    <div class="pizza-list">
+        <? foreach ($view->list as $pizza) : ?>
+            <div class="pizza-card">
+                <p class="title"><?= $pizza->name ?></p>
+                <p class="description"><?= $pizza->description ?></p>
+                <div class="btn-group">
+                    <a href="#modifier" class="btn">
+                        <i class="fa-solid fa-pen"></i>
+                        <span>Modifier</span>
+                    </a>
+                    <a href="#supprimer" class="btn red">
+                        <i class="fa-solid fa-trash"></i>
+                        <span>Supprimer</span>
+                    </a>
+                </div>
+                <div class="background">
+                    <div class="frame"></div>
+                    <img src="<?= $pizza->imageUrl ?>" class="img" />
+                </div>
+            </div>
+        <? endforeach ?>
+    </div>
 </div>
 
 
