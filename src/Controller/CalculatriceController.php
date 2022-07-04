7b0759d65cf0db2ea3179fe156963aaf15c3b6a7
David Jegat <david.jegat@gmail.com>
2022-07-04 16:09:39 +0200

    Correction de l'exercice des pages de la calculatrice

--- src/Controller/CalculatriceController.php
+++ src/Controller/CalculatriceController.php
@@ -14,4 +14,28 @@ class CalculatriceController extends AbstractController
     {
         return $this->render('calculatrice/index.html.twig');
     }
+
+    #[Route('/additionner')]
+    public function additionner(): Response
+    {
+        return $this->render('calculatrice/additionner.html.twig');
+    }
+
+    #[Route('/soustraire')]
+    public function soustraire(): Response
+    {
+        return $this->render('calculatrice/soustraire.html.twig');
+    }
+
+    #[Route('/diviser')]
+    public function diviser(): Response
+    {
+        return $this->render('calculatrice/diviser.html.twig');
+    }
+
+    #[Route('/multiplier')]
+    public function multiplier(): Response
+    {
+        return $this->render('calculatrice/multiplier.html.twig');
+    }
 }
