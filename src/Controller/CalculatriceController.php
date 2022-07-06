03c017598f96f79e73faf56b206f72d6170510bd
David Jegat <david.jegat@gmail.com>
2022-07-06 15:28:16 +0200

    Correction interactive : Ajout des operations dans le controller

--- src/Controller/CalculatriceController.php
+++ src/Controller/CalculatriceController.php
@@ -27,4 +27,74 @@ class CalculatriceController extends AbstractController
             'total' => $total,
         ]);
     }
+
+    #[Route('/calculatrice/soustraire/{x}/{y}', name: 'app_calculatrice_soustraire')]
+    public function soustraire(int $x, int $y): Response
+    {
+        // Calcule du résultat
+        $total = $x - $y;
+
+        // affichage de la page html
+        return $this->render('calculatrice/soustraire.html.twig', [
+            'x' => $x,
+            'y' => $y,
+            'total' => $total,
+        ]);
+    }
+
+    #[Route('/calculatrice/multiplier/{x}/{y}', name: 'app_calculatrice_multiplier')]
+    public function multiplier(int $x, int $y): Response
+    {
+        // Calcule du résultat
+        $total = $x * $y;
+
+        // affichage de la page html
+        return $this->render('calculatrice/multiplier.html.twig', [
+            'x' => $x,
+            'y' => $y,
+            'total' => $total,
+        ]);
+    }
+
+    #[Route('/calculatrice/diviser/{x}/{y}', name: 'app_calculatrice_diviser')]
+    public function diviser(int $x, int $y): Response
+    {
+        // Calcule du résultat
+        $total = $x / $y;
+
+        // affichage de la page html
+        return $this->render('calculatrice/diviser.html.twig', [
+            'x' => $x,
+            'y' => $y,
+            'total' => $total,
+        ]);
+    }
+
+    #[Route('/calculatrice/puissance/{x}/{y}', name: 'app_calculatrice_puissance')]
+    public function puissance(int $x, int $y): Response
+    {
+        // Calcule du résultat
+        $total = pow($x, $y);
+
+        // affichage de la page html
+        return $this->render('calculatrice/puissance.html.twig', [
+            'x' => $x,
+            'y' => $y,
+            'total' => $total,
+        ]);
+    }
+
+    #[Route('/calculatrice/modulo/{x}/{y}', name: 'app_calculatrice_modulo')]
+    public function modulo(int $x, int $y): Response
+    {
+        // Calcule du résultat
+        $total = $x % $y;
+
+        // affichage de la page html
+        return $this->render('calculatrice/puissance.html.twig', [
+            'x' => $x,
+            'y' => $y,
+            'total' => $total,
+        ]);
+    }
 }
