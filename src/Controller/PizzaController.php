6af5251374bb347913557da43af4cf146888c39a
David Jegat <david.jegat@gmail.com>
2022-07-08 14:12:29 +0200

    Correction du CRUD des pizzas : Modification et suppression

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -51,4 +51,50 @@ class PizzaController extends AbstractController
 
         return $this->render('pizza/newPizza.html.twig');
     }
+
+    #[Route('/pizza/{id}/modifier', name: 'app_pizza_update')]
+    public function update(int $id, PizzaRepository $repository, Request $request): Response
+    {
+        // J'aimerais récupérer la pizza avec l'id reçu plus haut
+        $pizza = $repository->find($id);
+
+        // Si le formulaire a été envoyé
+        if ($request->isMethod('POST')) {
+            // Récupération des données du formulaire
+            $name = $request->request->get('name');
+            $price = $request->request->get('price');
+            $description = $request->request->get('description');
+            $imageUrl = $request->request->get('imageUrl');
+
+            // Modification de la pizza avec les données du formulaire
+            $pizza->setName($name);
+            $pizza->setDescription($description);
+            $pizza->setPrice((float)$price);
+            $pizza->setImageUrl($imageUrl);
+
+            // Utiliser le repository afin d'enregistrer la pizza en base de données
+            $repository->add($pizza, true);
+
+            // Si tout c'est bien passé, on redirige vers la liste des pizzas
+            return $this->redirectToRoute('app_pizza');
+        }
+
+        // Afiicher le formulaire d'édition de la pizza
+        return $this->render('pizza/update.html.twig', [
+            'pizza' => $pizza,
+        ]);
+    }
+
+    #[Route('/pizza/{id}/supprimer', name: 'app_pizza_remove')]
+    public function remove(int $id, PizzaRepository $repository): Response
+    {
+        // Je récupére la pizza depuis la base de données
+        $pizza = $repository->find($id);
+
+        // Je supprime la pizza
+        $repository->remove($pizza, true);
+
+        // Je redirige vers la liste des pizzas
+        return $this->redirectToRoute('app_pizza');
+    }
 }
