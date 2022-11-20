ae7e1a48a3eacd3e1a45ae5561abd7b2f48ff57d
David Jegat <david.jegat@gmail.com>
2022-11-20 21:54:15 +0100

    Route

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,49 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
     /**
-     * Afin d'utiliser l'objet Symfony Request (représentant la Request HTTP)
-     * il faut tout d'abord l'injécter (l'envoyer en paramètre de notre
-     * controller)
+     * Création d'une route avec un paramètre.
+     * 
+     * Les paramètres de routes sont très utile ! Ils permettent 
+     * de rendre dynamique une partie d'une URI. Ici, essayer de vous rendre suir la page  "/hello/jean" !
+     * 
+     * Pour utiliser un paramètre de route il faut tout d'abord le déclarer
+     * dans l'URI de la route (ici "/hello/{name}"). Un paramètre posséde
+     * un nom et c'est ce dernier qui est utilisé afin de le récupérer !
      */
-    #[Route('/hello', name: 'app_hello_hello', methods: ['GET', 'POST'])]
-    public function index(Request $request): Response
+    #[Route('/hello/{name}', name: 'app_hello_someone', methods: ['GET', 'POST'])]
+    public function someone(string $name): Response
     {
         /**
-         * Grâce à l'bjet Request injécté plus haut, symfony met à disposition
-         * toutes les informations relatives à la Request HTTP !
+         * Ici le paramétre de controller $name provient de la route !
          * 
-         * Vous trouverez ci dessous les cas d'utilisation les plus répandu,
-         * cependant rien ne vaut la documentation officiel :
-         * 
-         * https://symfony.com/doc/current/components/http_foundation.html#request
+         * Symfony est assez inteligent pour faire le lien entre
+         * la string $name et le paramètre de la route {name} puisque
+         * ces derniers sont nommés à l'identique !
          */
-
-        // Récupération et test de la méthod HTTP 
-        $request->isMethod(Request::METHOD_GET);
-        $method = $request->getMethod();
-
-        // Récupération des « Queries » de l'url (les filtres)
-        if ($request->query->has('id')) {
-            $request->query->get('id');
-        }
-
-        // Récupération des données de formulaire
-        if ($request->request->has('email') && $request->request->has('password')) {
-            $email = $request->request->get('email');
-            $password = $request->request->get('password');
-        }
-
-        // récupération des cookies
-        $request->cookies->has('userId');
-        $request->cookies->get('userId');
-
-        return new Response('hello');
+        return new Response('Bonjour ' . $name);
     }
 }
