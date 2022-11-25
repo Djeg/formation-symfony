3de93f3ec482b69b7f384c2cde6e085c57cccd7c
David Jegat <david.jegat@gmail.com>
2022-11-25 11:40:35 +0100

    Add alice in all mode

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
-    Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
+    Hautelook\AliceBundle\HautelookAliceBundle::class => ['all' => true],
 ];
